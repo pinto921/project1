9a2146a3bee34576db0b9388f29bbd91923acd96
pinto921 <pinto921@hotmail.com>
2016-09-11 00:00:58 +0000

    task 3 done

---
+++ is210-week-02-warmup-master/is210-week-02-synthesizing-master/task_03.py
@@ -0,0 +1,16 @@
+
+-CHARLIE = "chocolate factory"
++#!/usr/bin/env python
++# -*- coding: utf-8 -*-
++"""this is a docstring"""
++
++CHARLIE = "Chocolate Factory" 
+VIOLET = "Gray"
+PATRICIA = "Reichardt"
+LINUS = "van Pelt"
+-
+-
+-
+
+
+
